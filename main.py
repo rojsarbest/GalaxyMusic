734f03a82978d0c16108969d912b4c7739f89570
rojsarbest <86238771+rojsarbest@users.noreply.github.com>
2021-06-22 18:47:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
+import requests
 from pyrogram import Client as Bot
 
 from callsmusic import run
